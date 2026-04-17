7a884019d3b1fe7125070122bf5bf3681e251b0b
Naila <naelazure@gmail.com>
2026-04-17 11:24:34 +0700

    Update soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Masukkan n: ";
+    cin >> n;
+    
+    cout << "H";
+    for (int i = 0; i < n; i++){
+        cout << "o";
+    } cout << "r";
+    for (int i = 0; i < n; i++){
+        cout << "e";
+    } for (int i = 0; i < n; i++){
+        cout << "!";
+    }
+    
+    return 0;
+}
+
+//User memasukkan n yang nantinya akan digunakan untuk loop o, e dan tanda seru
+//Cetak H, cetak o sebanyak n kali, cetak r, cetak e dan ! sebanyak n kali
