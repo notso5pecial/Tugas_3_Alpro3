2f4d2a4d3f1cf5feefaef773e93b0021e17893c0
Naila <naelazure@gmail.com>
2026-04-17 11:57:25 +0700

    Update soal6.cpp

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n, total = 0; 
+    cout << "Masukkan n: ";
+    cin >> n;
+    
+    for (int i = 2; i <= n; i += 2) { 
+        total = total + (i * i); 
+    }
+    
+    cout << "Total = " << total;
+    return 0;
+}
+
+//deklarasi total=0 dulu biar nilai awalnya ada
+//dari i = 2, selama i lebih kecil dari n maka akan dioperasikan, i+=2 karena yang dihitung hanya bilangan genap sampai n, loop terus sampai n
